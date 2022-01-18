653fc44bc7f2bccc9a0f861bcf82ad279f945abb
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-18 22:21:11 +0900

    tmp

--- notepad/app/src/main/java/com/example/notepad/DisplayMessageActivity.java
+++ notepad/app/src/main/java/com/example/notepad/DisplayMessageActivity.java
@@ -1,7 +1,11 @@
 package com.example.notepad;
 
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowInsets;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        // Get the Intent that started this activity and extract the String
+        String message = getIntent().getStringExtra(EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the String as its text
+        String textView = "hello";
     }
 }
