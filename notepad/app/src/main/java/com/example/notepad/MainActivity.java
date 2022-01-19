95ca0c9039a94f27328f05f74bdb34a5874078df
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-19 17:31:34 +0900

    試してみる(https://takaittakait.hatenablog.com/entry/20210823/1629676800#SubActivity)

--- notepad/app/src/main/java/com/example/notepad/MainActivity.java
+++ notepad/app/src/main/java/com/example/notepad/MainActivity.java
@@ -7,19 +7,12 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_DATA = "com.example.notepad.DATA";
-    public static String data1 = "Hello World!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button sendButton = findViewById(R.id.button);
-        sendButton.setOnClickListener(v -> {
-            Intent intent = new Intent(getApplication(), DisplayMessageActivity.class);
-            intent.putExtra(EXTRA_DATA, data1);
-            startActivity(intent);
-        });
     }
+
+
 }
