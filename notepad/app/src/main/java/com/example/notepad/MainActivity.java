c9c3e30e2cac5f3d57683ffaa6766ec6619fc339
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-19 17:35:45 +0900

    無理っぽい．今度はこっち（https://codeforfun.jp/android-studio-how-to-set-button-click-event/）

--- notepad/app/src/main/java/com/example/notepad/MainActivity.java
+++ notepad/app/src/main/java/com/example/notepad/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.notepad;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu Menu) {
+        getMenuInflater().inflate(R.layout.activity_display_message, menu);
+    }
 }
