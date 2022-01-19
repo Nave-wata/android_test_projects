fd978fd66b1725b75e0e8bd00b17ba9b1f47dbc0
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-19 18:17:19 +0900

    tmp

--- notepad/app/src/main/java/com/example/notepad/MainActivity.java
+++ notepad/app/src/main/java/com/example/notepad/MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.notepad;
 
 import android.content.Intent;
-import android.os.Parcelable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.view.Menu;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
