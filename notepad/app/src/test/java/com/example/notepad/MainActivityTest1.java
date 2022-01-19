a5a251fc85ca849d3e7dbc96dccc14d7a6497ccc
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-19 18:39:28 +0900

    テストコード書き方的なの(https://qiita.com/izuki_y/items/d784529c301af2883b85)

---
+++ notepad/app/src/test/java/com/example/notepad/MainActivityTest1.java
@@ -0,0 +1,24 @@
+package com.example.notepad;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainActivityTest1 {
+
+    private MainActivity mainclass;
+
+    @Before
+    public void setUp() throws Exception {
+        mainclass = new MainActivity();
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void sendMessage() {
+    }
+}
