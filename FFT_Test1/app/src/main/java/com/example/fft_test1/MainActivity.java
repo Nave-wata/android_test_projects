1a4720d9ba7f3b6913c57719738ed73758735ca0
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-22 20:58:34 +0900

    コピー+ちょっと調節（動かない） http://web-dou.com/android/fft.html

--- FFT_Test1/app/src/main/java/com/example/fft_test1/MainActivity.java
+++ FFT_Test1/app/src/main/java/com/example/fft_test1/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.fft_test1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import java.math.*;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.Log;
+import android.view.View;
+import java.io.IOException;
+import android.content.Context;
+import org.jtransforms.fft.DoubleFFT_1D;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int FFT_SIZE = 8 ;
+        double[] data ;
+        data = new double[FFT_SIZE];
+
+        // オリジナルデータを出力
+        for ( int i=0; i<FFT_SIZE; i++ )
+        {
+            data[i] = 10.0 + i ;
+            Log.d("ORG", "i=" + i + " val=" +data[i] ) ;
+        }
+
+        DoubleFFT_1D fft = new DoubleFFT_1D(FFT_SIZE) ;
+
+        // フーリエ変換(FFT)の実行
+        fft.realForward(data);
+        // data[0]は実数成分、data[1]は虚数成分～data[n]は実数成分、data[n+1}は虚数成分
+        for ( int i=0; i<FFT_SIZE; i++ )
+        {
+            // フーリエ変換後のデータを出力
+            Log.d("FFT", "i=" + i + " val=" + data[i] ) ;
+        }
+
+        // 逆フーリエ変換
+        fft.realInverse(data,  true) ;
+        for ( int i=0; i<FFT_SIZE; i++ )
+        {
+            // 逆フーリエ変換後のデータを出力
+            Log.d("IFFT", "i=" + i + " val=" + data[i] ) ;
+        }
     }
+
 }
