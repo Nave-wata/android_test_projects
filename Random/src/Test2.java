f92057aa8c32cd5b028648b4b1782b9dacc6aac3
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-23 02:44:24 +0900

    差取得

--- Random/src/Test2.java
+++ Random/src/Test2.java
@@ -3,7 +3,7 @@ public class Test2 {
     static int bufSize = 1024; // フーリエ変換のサイズと同じ
     static int[] Base_Hz = { 43, 86, 129, 258, 516, 1033, 2024, 4005, 8010, 16020 };
     static int[] Base_Hz_cnt = { 1, 2, 3, 7, 13, 25, 48, 94, 187, 373 }; // 全部で 512 個
-    static int[] Base_Hz_cnt_sub;
+    static int[] Base_Hz_cnt_sub = new int[bufSize];
     static double[] VolUp = new double[bufSize / 2];
 
     public static void main(String[] args) {
@@ -15,17 +15,19 @@ public class Test2 {
         }
 
         for (i = 0; i < 10; i++) {
-            sub = (Base_Hz_cnt_sub[i + 1] - 1) - Base_Hz_cnt[i];
-            System.out.println(sub);
             if (i != 9) {
+                sub = (Base_Hz_cnt[i + 1] - 1) - Base_Hz_cnt[i];
+                System.out.println(sub);
                 for (j = Base_Hz_cnt[i]; j < Base_Hz_cnt[i + 1]; j++) {
 
                 }
-                for (j = Base_Hz_cnt[i + 1]; j < Base_Hz_cnt[i]; j++) {
+                for (j = Base_Hz_cnt[i + 1]; j < Base_Hz_cnt[i]; j--) {
 
                 }
                 // System.out.println(Base_Hz_cnt[i] + " ~ " + Base_Hz_cnt[i + 1]);
             } else {
+                sub = (512 - 1) - Base_Hz_cnt[i];
+                System.out.println(sub);
                 for (j = Base_Hz_cnt[i]; j < 512; j++) {
 
                 }
