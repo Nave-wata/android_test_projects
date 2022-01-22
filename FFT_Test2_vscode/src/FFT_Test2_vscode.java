e1bc0c862dae09f91e3b83339bcf75d08936d7ab
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-23 00:24:54 +0900

    FFTできたぞー

--- FFT_Test2_vscode/src/FFT_Test2_vscode.java
+++ FFT_Test2_vscode/src/FFT_Test2_vscode.java
@@ -1,24 +1,33 @@
 import org.jtransforms.fft.DoubleFFT_1D;
 
 public class FFT_Test2_vscode {
-    static DoubleFFT_1D fft;
     static int FFT_SIZE = 8;
     static double[] data = new double[FFT_SIZE];
 
     public static void main(String[] args) throws java.lang.NoClassDefFoundError {
         for (int i = 0; i < FFT_SIZE; i++) {
             data[i] = 10.0 + i;
-            System.out.println("data[" + i + "] = " + data[i]);
+            System.out.println("ORG:  i = " + i + ", val = " + data[i]);
         }
         System.out.println();
 
-        fft = new DoubleFFT_1D(FFT_SIZE);
+        DoubleFFT_1D fft = new DoubleFFT_1D(FFT_SIZE);
 
-        try {
-            fft.realForward(data);
-        } catch (NoClassDefFoundError e) {
-            System.out.println("fft.realForward(data)");
+        // フーリエ変換(FFT)の実行
+        fft.realForward(data);
+        // data[0]は実数成分、data[1]は虚数成分～data[n]は実数成分、data[n+1}は虚数成分
+        for (int i = 0; i < FFT_SIZE; i++) {
+            // フーリエ変換後のデータを出力
+            System.out.println("FFT:  i = " + i + ", val = " + data[i]);
         }
+        System.out.println();
 
+        // 逆フーリエ変換
+        fft.realInverse(data, true);
+        for (int i = 0; i < FFT_SIZE; i++) {
+            // 逆フーリエ変換後のデータを出力
+            System.out.println("IFFT: i = " + i + ", val = " + data[i]);
+        }
+        System.out.println();
     }
 }
