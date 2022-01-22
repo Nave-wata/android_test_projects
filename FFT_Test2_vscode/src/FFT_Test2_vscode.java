eb6b7de5053a341f0c19b78df0117df1173f7f78
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-22 23:13:09 +0900

    やっぱり動かん

--- FFT_Test2_vscode/src/FFT_Test2_vscode.java
+++ FFT_Test2_vscode/src/FFT_Test2_vscode.java
@@ -1,9 +1,19 @@
 import org.jtransforms.fft.DoubleFFT_1D;
 
 public class FFT_Test2_vscode {
-    DoubleFFT_1D fft;
+    static DoubleFFT_1D fft;
+    static int FFT_SIZE = 8;
+    static double[] data = new double[FFT_SIZE];
 
     public static void main(String[] args) {
-        System.out.println("FFT_Test2_vscode");
+        for (int i = 0; i < FFT_SIZE; i++) {
+            data[i] = 10.0 + i;
+            System.out.println("data[" + i + "] = " + data[i]);
+        }
+        System.out.println();
+
+        fft = new DoubleFFT_1D(FFT_SIZE);
+
+        fft.realForward(data);
     }
 }
