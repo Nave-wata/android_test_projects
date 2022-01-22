02b8417b1f08ed139d06ef2a4aff6fc8eea2f552
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-23 02:14:16 +0900

    基本の周波数とそれが何個目か 0スタートやからBase_Hz_cntは配列用

--- Random/src/Test2.java
+++ Random/src/Test2.java
@@ -1,5 +1,41 @@
 public class Test2 {
+    static int SAMPLING_RATE = 44100;
+    static int bufSize = 1024; // フーリエ変換のサイズと同じ
+    static int[] Base_Hz = { 43, 86, 129, 172, 258, 516, 1033, 2024, 4005, 8010, 16020 };
+    static int[] Base_Hz_cnt = { 1, 2, 3, 4, 6, 12, 24, 47, 93, 186, 372 };
+
     public static void main(String[] args) {
-        System.out.println("Test2.java");
+        int i;
+        int Hz;
+
+        for (i = 0; i < bufSize / 2; i++) {
+            Hz = i * SAMPLING_RATE / bufSize;
+
+            if (Hz == Base_Hz[0]) {
+                System.out.println(Base_Hz[0] + " = " + i);
+            } else if (Hz == Base_Hz[1]) {
+                System.out.println(Base_Hz[1] + " = " + i);
+            } else if (Hz == Base_Hz[2]) {
+                System.out.println(Base_Hz[2] + " = " + i);
+            } else if (Hz == Base_Hz[3]) {
+                System.out.println(Base_Hz[3] + " = " + i);
+            } else if (Hz == Base_Hz[4]) {
+                System.out.println(Base_Hz[4] + " = " + i);
+            } else if (Hz == Base_Hz[5]) {
+                System.out.println(Base_Hz[5] + " = " + i);
+            } else if (Hz == Base_Hz[6]) {
+                System.out.println(Base_Hz[6] + " = " + i);
+            } else if (Hz == Base_Hz[7]) {
+                System.out.println(Base_Hz[7] + " = " + i);
+            } else if (Hz == Base_Hz[8]) {
+                System.out.println(Base_Hz[8] + " = " + i);
+            } else if (Hz == Base_Hz[9]) {
+                System.out.println(Base_Hz[9] + " = " + i);
+            } else if (Hz == Base_Hz[10]) {
+                System.out.println(Base_Hz[10] + " = " + i);
+            } else {
+
+            }
+        }
     }
 }
