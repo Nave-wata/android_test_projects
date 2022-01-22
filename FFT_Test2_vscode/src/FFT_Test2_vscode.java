5f69a6e44e83598c354dd017dd7e2958093a38b4
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-23 00:32:55 +0900

    というわけで戻した

--- FFT_Test2_vscode/src/FFT_Test2_vscode.java
+++ FFT_Test2_vscode/src/FFT_Test2_vscode.java
@@ -2,7 +2,7 @@ import org.jtransforms.fft.DoubleFFT_1D;
 
 public class FFT_Test2_vscode {
     static int FFT_SIZE = 8;
-    static double[] data = new double[FFT_SIZE * 3];
+    static double[] data = new double[FFT_SIZE];
 
     public static void main(String[] args) throws java.lang.NoClassDefFoundError {
         for (int i = 0; i < FFT_SIZE; i++) {
@@ -16,7 +16,7 @@ public class FFT_Test2_vscode {
         // フーリエ変換(FFT)の実行
         fft.realForward(data);
         // data[0]は実数成分、data[1]は虚数成分～data[n]は実数成分、data[n+1]は虚数成分
-        for (int i = 0; i < data.length; i++) {
+        for (int i = 0; i < FFT_SIZE; i++) {
             // フーリエ変換後のデータを出力
             System.out.println("FFT:  i = " + i + ", val = " + data[i]);
         }
@@ -24,7 +24,7 @@ public class FFT_Test2_vscode {
 
         // 逆フーリエ変換
         fft.realInverse(data, true);
-        for (int i = 0; i < data.length; i++) {
+        for (int i = 0; i < FFT_SIZE; i++) {
             // 逆フーリエ変換後のデータを出力
             System.out.println("IFFT: i = " + i + ", val = " + data[i]);
         }
