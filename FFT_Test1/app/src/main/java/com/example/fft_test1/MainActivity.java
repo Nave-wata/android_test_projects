dd3b0fc73494229862a155330fff589a1f0d24c0
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-22 21:27:25 +0900

    このMainActivity.javaの30行目参照

--- FFT_Test1/app/src/main/java/com/example/fft_test1/MainActivity.java
+++ FFT_Test1/app/src/main/java/com/example/fft_test1/MainActivity.java
@@ -15,11 +15,10 @@ public class MainActivity extends Activity {
 
         int FFT_SIZE = 8 ;
         double[] data ;
-        data = new double[FFT_SIZE];
+        data = new double[FFT_SIZE * 2];
 
         // オリジナルデータを出力
-        for ( int i=0; i<FFT_SIZE; i++ )
-        {
+        for ( int i=0; i<FFT_SIZE; i++ ) {
             data[i] = 10.0 + i ;
             Log.d("ORG", "i=" + i + " val=" +data[i] ) ;
         }
@@ -29,16 +28,14 @@ public class MainActivity extends Activity {
         // フーリエ変換(FFT)の実行
         fft.realForward(data);
         // data[0]は実数成分、data[1]は虚数成分～data[n]は実数成分、data[n+1}は虚数成分
-        for ( int i=0; i<FFT_SIZE; i++ )
-        {
+        for ( int i=0; i<FFT_SIZE; i++ ) {
             // フーリエ変換後のデータを出力
             Log.d("FFT", "i=" + i + " val=" + data[i] ) ;
         }
 
         // 逆フーリエ変換
         fft.realInverse(data,  true) ;
-        for ( int i=0; i<FFT_SIZE; i++ )
-        {
+        for ( int i=0; i<FFT_SIZE; i++ ) {
             // 逆フーリエ変換後のデータを出力
             Log.d("IFFT", "i=" + i + " val=" + data[i] ) ;
         }
