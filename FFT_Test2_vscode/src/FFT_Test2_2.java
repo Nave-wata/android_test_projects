7fc3aa3caacb716e2f60a72687dc4940f03bb2ea
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-01-24 21:28:32 +0900

    できた？

--- FFT_Test2_vscode/src/FFT_Test2_2.java
+++ FFT_Test2_vscode/src/FFT_Test2_2.java
@@ -1,11 +1,14 @@
 import org.jtransforms.fft.DoubleFFT_1D;
+import java.util.*;
 
 public class FFT_Test2_2 {
     public static void main(String[] args) {
-        DoubleFFT_1D[] ffts;
+        ArrayList<DoubleFFT_1D> ffts = new ArrayList<DoubleFFT_1D>();
+        ffts.add(DoubleFFT_1D(5));
+    }
 
-        for (int i = 0; i < 5; i++) {
-            ffts[i] = new DoubleFFT_1D(10);
-        }
+    private static DoubleFFT_1D DoubleFFT_1D(int i) {
+        DoubleFFT_1D fft = new DoubleFFT_1D(5);
+        return fft;
     }
 }
